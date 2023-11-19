e40b61f0f404e5e0c449a8470080522f4b757563
JhoZzel <joel.lol177@gmail.com>
2023-11-19 17:55:29 -0500

    Crando el metodo all_rating en la clase movie

--- rottenpotatoes-rails-intro/app/models/movie.rb
+++ rottenpotatoes-rails-intro/app/models/movie.rb
@@ -1,3 +1,8 @@
+# app/models/movie.rb
 class Movie < ActiveRecord::Base
-
+  def self.all_ratings
+    # Devuelve un array con todas las clasificaciones posibles
+    ['G', 'PG', 'PG-13', 'R']
+  end
 end
+
