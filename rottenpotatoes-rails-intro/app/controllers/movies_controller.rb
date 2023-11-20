05d7c9d6db79df13865efe1597901dbc741b18e5
Angellopp <angellojamir@hotmail.com>
2023-11-20 00:13:46 -0500

    Creacion de Session

--- rottenpotatoes-rails-intro/app/controllers/movies_controller.rb
+++ rottenpotatoes-rails-intro/app/controllers/movies_controller.rb
@@ -12,24 +12,8 @@ class MoviesController < ApplicationController
     # Configurar las clasificaciones seleccionadas para que las casillas de verificación se muestren como marcadas
     @ratings_to_show = params[:ratings] || session[:ratings] || []
 
-    # Obtener todas las clasificaciones posibles para construir las casillas de verificación
+    # Obtenemos todas las clasificaciones posibles para construir las casillas de verificación
     @all_ratings = Movie.all_ratings
-
-    # Verifica si no se pasaron parámetros de clasificación o filtrado en la URL
-    if params[:sort_by].nil? && params[:ratings].nil?
-      # Recupera las configuraciones de clasificación y filtrado desde session[]
-      # Esto restaurará las configuraciones previas almacenadas en las cookies
-      params[:sort_by] = session[:sort_by] if session[:sort_by].present?
-      params[:ratings] = session[:ratings] if session[:ratings].present?
-
-      # Redirige a la URL con las configuraciones almacenadas en session[]
-      redirect_to movies_path(params)
-      return
-    end
-
-    # Guarda las configuraciones de clasificación y filtrado en session[]
-    session[:sort_by] = params[:sort_by]
-    session[:ratings] = params[:ratings]
 
     # Verifica si se proporciona un orden y configura las variables de control
     if params[:order].present?
@@ -39,9 +23,10 @@ class MoviesController < ApplicationController
     end
 
     # Filtramos las películas por clasificaciones seleccionadas
-    if params[:ratings].present?
+    if params[:ratings].present? || session[:ratings].present?
       selected_ratings = params[:ratings].keys
       @movies = @movies.with_ratings(selected_ratings)
+      session[:ratings] = selected_ratings
     end
   end
 
